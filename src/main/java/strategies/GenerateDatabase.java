56a3e1c04c94ccd6c50e2efe3556984a4dbd63a3
Luan <luan.venancio04@hotmail.com>
2023-09-20 17:29:38 -0300

    Fix Generate Database

--- src/main/java/strategies/GenerateDatabase.java
+++ src/main/java/strategies/GenerateDatabase.java
@@ -1,6 +1,6 @@
 package strategies;
 
- import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Date;
 
 import com.tsi.finance.simulator.FileManager;
@@ -12,33 +12,33 @@ import com.tsi.finance.simulator.Strategy;
  * @author luanv
  */
 public class GenerateDatabase implements Strategy {
-    
+
     private double precoAmanha;
     private double precoHoje;
     private int posicao;
     private ArrayList<MarketData> historico;
-    
+
     public GenerateDatabase() {
         this.precoHoje = 0;
     }
 
     @Override
     public void create() {
-        
+
         System.out.println("Creating the strategy - Simple Strategy");
-        
+
         String header = "Open,High,Low,Close,Volume,Output\n";
-        FileManager.writer("./market_data/Database.csv", header);
-        
+        FileManager.writerAppend("./market_data/Database.csv", header);
+
         this.posicao = 0;
         this.historico = new ArrayList<>();
     }
 
     @Override
     public void start(Date date) {
-        
+
         System.out.println("[Simple Strategy] Starting - " + date.toString());
-        
+
     }
 
     @Override
@@ -46,30 +46,27 @@ public class GenerateDatabase implements Strategy {
 
         /*
             PROCESSING THE MARKET DATA TO TAKE DECISIONS
-        */
-
+         */
         this.precoAmanha = update.getClose();
-        
+
         System.out.println("[Simple Strategy] MD - " + update.toString());
-        if(posicao > 0) {
+        if (posicao > 0) {
             Double retorno = precoAmanha - precoHoje;
-            String data = historico.get(historico.size()-1).getOpen() + "," + historico.get(historico.size() -1).getHigh() + "," +  historico.get(historico.size() -1).getLow() + "," +  historico.get(historico.size() -1).getClose() + "," +  historico.get(historico.size() -1).getVolume() + "," + retorno + "\n";
-            FileManager.writerAppend("./market_data/Base.csv", data);
+            String data = historico.get(historico.size() - 1).getOpen() + "," + historico.get(historico.size() - 1).getHigh() + "," + historico.get(historico.size() - 1).getLow() + "," + historico.get(historico.size() - 1).getClose() + "," + historico.get(historico.size() - 1).getVolume() + "," + retorno + "\n";
+            FileManager.writerAppend("./market_data/Database.csv", data);
         }
 
         posicao++;
 
         //this.precoAmanha = update.getClose();
-        this.precoHoje=update.getClose();
+        this.precoHoje = update.getClose();
         this.historico.add(update);
     }
 
     @Override
     public void finish(Date date) {
-        
-        System.out.println("[Simple Strategy] Finishing - " + date.toString());
-        
+
+        //System.out.println("[Simple Strategy] Finishing - " + date.toString());
     }
-    
-  
+
 }
